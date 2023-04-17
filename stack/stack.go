503cc4e4e82d579703d3e53cc81876c616a0cf77
Qingyun Hunang <qingyun.huang@ubitus.net>
2023-04-17 18:04:08 +0800

    add stack

---
+++ stack/stack.go
@@ -0,0 +1,28 @@
+package stack
+
+type Stack[T any] struct {
+	slice []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		slice: []T{},
+	}
+}
+
+func (stk *Stack[T]) Push(key T) {
+	stk.slice = append(stk.slice, key)
+}
+
+func (stk *Stack[T]) Pop() (T, bool) {
+	var x T
+	if len(stk.slice) > 0 {
+		x, stk.slice = stk.slice[len(stk.slice)-1], stk.slice[:len(stk.slice)-1]
+		return x, true
+	}
+	return x, false
+}
+
+func (stk *Stack[T]) IsEmpty() bool {
+	return len(stk.slice) == 0
+}
