60160932362075ad917d2bc45629ef083221cd70
qingyun <rivendinner@gmail.com>
2023-04-22 02:04:09 +0800

    prefix_tree

--- data_structure/prefix_tree/prefix_tree.go
+++ data_structure/prefix_tree/prefix_tree.go
@@ -1,36 +1,26 @@
 package prefixtree
 
-// leaf can be string
-// array
-
-// char-dict
 type Trie struct {
 	children map[byte]*Trie
+	isWord   bool
 }
 
-func Init(strs []string) *Trie {
-	t := &Trie{}
-	for i := 0; i < len(strs); i++ {
-		t.Insert(strs[i])
+func NewTrie() *Trie {
+	return &Trie{
+		children: make(map[byte]*Trie),
 	}
-
-	return t
 }
 
 func (t *Trie) Insert(s string) {
-	root := t
+	var c byte
 	for i := 0; i < len(s); i++ {
-		c := s[i]
-		if child, ok := root.children[c]; ok {
-			root = child
-		} else {
-			root.children[c] = &Trie{
-				children: make(map[byte]*Trie),
-			}
-
-			root = root.children[c]
+		c = s[i]
+		if _, ok := t.children[c]; !ok {
+			t.children[c] = NewTrie()
 		}
+		t = t.children[c]
 	}
+	t.isWord = true
 }
 
 func (t *Trie) Remove(s string) {
@@ -38,28 +28,47 @@ func (t *Trie) Remove(s string) {
 		return
 	}
 
-	// find
-	root := t
-	for i := 0; i < len(s)-1; i++ {
-		if child, ok := root.children[s[i]]; ok {
-			root = child
+	// if do not find, return
+	// if find in middle layer, change isWord = false
+	// if find in leaf, delete node and back to delete leaf
+
+	path := []*Trie{t}
+	var c byte
+	for i := 0; i < len(s); i++ {
+		c = s[i]
+		if child, ok := t.children[c]; ok {
+			path = append(path, child)
+			t = child
 		} else {
 			return
 		}
 	}
 
-	delete(root.children, s[s[len(s)-1]])
+	if len(t.children) != 0 {
+		t.isWord = false
+		return
+	}
+
+	i, j := len(s)-1, len(path)-2
+	for {
+		delete(path[j].children, s[i])
+		if len(path[j].children) == 0 && !path[j].isWord {
+			i--
+			j--
+		} else {
+			break
+		}
+	}
 }
 
 func (t *Trie) Search(s string) bool {
-	root := t
 	for i := 0; i < len(s); i++ {
-		if child, ok := root.children[s[i]]; ok {
-			root = child
+		if child, ok := t.children[s[i]]; ok {
+			t = child
 		} else {
 			return false
 		}
 	}
 
-	return true
+	return t.isWord
 }
