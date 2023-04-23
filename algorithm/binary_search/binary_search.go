be6e94ad0707f5c4cf17dfa8dd5c4bb4763bce93
qingyun <rivendinner@gmail.com>
2023-04-23 15:43:44 +0800

    add bsearch

--- algorithm/binary_search/binary_search.go
+++ algorithm/binary_search/binary_search.go
@@ -1,1 +1,17 @@
 package binarysearch
+
+// find i where nums[i] < comp && nums[i+1] >= comp
+func FindLastSmall(nums []int, comp int) int {
+	l, r := 0, len(nums)-1
+	var m int
+	for l <= r {
+		m = (l + r) >> 1
+		if nums[m] < comp {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+
+	return r
+}
