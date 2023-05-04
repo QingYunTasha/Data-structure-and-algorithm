dcfd7ee78d617e7e0ef867869aa6c276324f2590
qingyun <rivendinner@gmail.com>
2023-05-05 00:18:23 +0800

    x

--- data_structure/binary_indexed_tree/binary_indexec_tree.go
+++ data_structure/binary_indexed_tree/binary_indexec_tree.go
@@ -1,1 +1,40 @@
 package binaryindexedtree
+
+/*
+Use: Compute dynamic prefix sum
+Complexity:
+	Time: O(logN)
+	Space: O(N)
+*/
+
+type binaryIndexedTree []int
+
+func NewBinaryIndexedTree(length int) binaryIndexedTree {
+	return make(binaryIndexedTree, length)
+}
+
+// Set A[i] += v
+func (t binaryIndexedTree) Update(i, v int) {
+	n := len(t)
+	for i <= n {
+		t[i]
+	}
+}
+
+// Set A[i] = v
+func (t binaryIndexedTree) Set(i, v int) {
+	n := len(t)
+	for i <= n {
+		t[i]
+	}
+}
+
+// A[0] + ... + A[i]
+func (t binaryIndexedTree) QueryPrefixSum(i int) {
+
+}
+
+// B[i] - B[i-1]
+func (t binaryIndexedTree) Query(i int) {
+
+}
