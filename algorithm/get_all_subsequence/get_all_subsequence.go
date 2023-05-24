142724f3e522c7f052f40c56061043215ece5de6
qingyun <rivendinner@gmail.com>
2023-05-24 22:59:05 +0800

    get all subsequence

---
+++ algorithm/get_all_subsequence/get_all_subsequence.go
@@ -0,0 +1,24 @@
+package getallsubsequence
+
+/* @tags: subsequence,permutation */
+
+func GetSubsequences(nums []int, k int) [][]int {
+	result := [][]int{}
+	GenerateSubsequences(nums, k, 0, []int{}, &result)
+	return result
+}
+
+func GenerateSubsequences(nums []int, k int, start int, current []int, result *[][]int) {
+	if len(current) == k {
+		temp := make([]int, len(current))
+		copy(temp, current)
+		*result = append(*result, temp)
+		return
+	}
+
+	for i := start; i < len(nums); i++ {
+		current = append(current, nums[i])
+		GenerateSubsequences(nums, k, i+1, current, result)
+		current = current[:len(current)-1]
+	}
+}
