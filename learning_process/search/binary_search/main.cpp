52297240d01e154c8ab13213e04156e7837bdc4a
qingyun <rivendinner@gmail.com>
2023-06-15 23:11:44 +0800

    add

--- learning_process/search/binary_search/main.cpp
+++ learning_process/search/binary_search/main.cpp
@@ -158,3 +158,27 @@ pair<int, int> findInterval(vector<int>& nums, int target){
         
     return make_pair(r, l);
 }
+
+/*
+300. Longest Increasing Subsequence
+*/
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> temp;
+
+    temp.push_back(nums[0]);
+    for(int i=1; i<n; i++){
+
+        if(nums[i] > temp.back()){
+
+            temp.push_back(nums[i]);
+        }
+        else{
+            int it = lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin();
+            
+            temp[it] = nums[i];
+        }
+    }
+
+    return temp.size();
+}
